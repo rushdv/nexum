56c69f001c86385357c6ed377475227588d97672
Md Shihab Shahriar Rashu <md.shihabshahriarrashu@gmail.com>
2026-06-07 22:17:37 +0600

    feat: wire notifications and socket.io into comment controller

--- backend/src/controllers/comment.controller.js
+++ backend/src/controllers/comment.controller.js
@@ -1,4 +1,6 @@
 import CommentModel from "../models/comment.model.js";
+import PostModel from "../models/post.model.js";
+import NotificationModel from "../models/notification.model.js";
 
 export const createComment = async (req, res, next) => {
   try {
@@ -9,6 +11,25 @@ export const createComment = async (req, res, next) => {
     if (!content) return res.status(400).json({ message: "Content is required" });
 
     const comment = await CommentModel.create({ userId, postId, content });
+
+    const post = await PostModel.findById(postId);
+    if (post) {
+      await NotificationModel.create({
+        userId: post.user_id,
+        actorId: userId,
+        postId: Number(postId),
+        type: "comment"
+      });
+      const io = req.app.get("io");
+      if (io) {
+        io.to(`user:${post.user_id}`).emit("notification", {
+          type: "comment",
+          actorId: userId,
+          postId: Number(postId),
+        });
+      }
+    }
+
     res.status(201).json(comment);
   } catch (err) {
     next(err);
